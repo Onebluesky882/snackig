26871269d863fb18d1b16b224a9e4c7ed33e78df
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2025-03-02 14:52:32 +0700

    install router

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";
+const Homescreen = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>Homescreen</Text>
+    </View>
+  );
+};
+export default Homescreen;
+const styles = StyleSheet.create({});
